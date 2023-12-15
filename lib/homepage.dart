e0374dfa3f9b80abe9057513cbcf0893ecab6680
0122345 <ntwarifiacre043@gmail.com>
2023-12-15 13:52:53 +0200

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+    backgroundColor: Colors.grey[900],
+    appBar: AppBar(
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      leading: Icon(Icons.menu),
+      actions: [Icon(Icons.person)],
+    ),
+    bottomNavigationBar: BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: '',
+            ),
+             BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: '',
+            ),
+      ]),
+    body: Column(
+    
+    ),
+    );
+  }
+}
